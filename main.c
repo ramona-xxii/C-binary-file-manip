fd6bf2175c6e79959b59c6edfbdad64a534b4893
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-09-16 12:03:57 -0400

    implemented menu selection for creating binary file

--- main.c
+++ main.c
@@ -1,45 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-    //student record structure
-    struct student_record
-    {
-        int studentID;
-        char studentName[50]; 
-        char emailID[50];
-        char courseID[50];
-        char grade[3];
-    };
+// student record structure
+struct student_record
+{
+    int studentID;
+    char studentName[50];
+    char emailID[50];
+    char courseID[50];
+    char grade[3];
+};
 
-int main(void){
-
+int main(void)
+{
     // variable declarations
-    int choice;
+    int menuSelection;
     FILE *fptr;
 
-    // display main menu
-    printf("\t\tMAIN MENU\n"
-    "1. Create the binary file\n"
-    "2. Add a record to the file\n"
-    "3. Display the contents of the file\n"
-    "4. Seek a specific record\n"
-    "5. Update contents of a record\n"
-    "6. Delete a record for a specific name\n"
-    "7. Exit\n"
-    "\t\tPlease enter your choice...");
+    do
+    {
+        // display main menu
+        printf("\t\tMAIN MENU\n"
+               "1. Create the binary file\n"
+               "2. Add a record to the file\n"
+               "3. Display the contents of the file\n"
+               "4. Seek a specific record\n"
+               "5. Update contents of a record\n"
+               "6. Delete a record for a specific name\n"
+               "7. Exit\n"
+               "\t\tPlease enter your choice... ");
 
-    // get & store user's choice
-    scanf("%d", &choice);
+        // get & store user's menu selection
+        scanf("%d", &menuSelection);
 
-    // determine choice made
-    switch(choice) {
-        case 1:
-            // create file
-            printf("Creating file...\n");
-            fptr = fopen("records.dat", "ab");
+        // determine menu selection
+        switch (menuSelection)
+        {
+
+        case 1: // CREATE FILE
+            
+            // check if file already exists
+            fptr = fopen("student-records.dat", "rb");
+            
+            if (fptr == NULL)
+            {
+                printf("Creating file 'student-records.dat'");
+                fptr = fopen("student-records.dat", "wb+");
+            }
+            else
+            {
+                printf("File 'student-records.dat' already exists.\n");
+                fclose(fptr);
+            }
             break;
-        case 2:
-            // add record to file
+
+        case 2: // ADD RECORD TO FILE
             break;
         case 3:
             // display file contents
@@ -59,9 +74,8 @@ int main(void){
             exit(0);
         default:
             printf("Please enter a valid choice...");
+        }
 
-    }
+    } while (menuSelection != 7);
 
-    
-    
 }
