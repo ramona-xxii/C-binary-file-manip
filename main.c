3477c8ad3f424312de5ce0ef3f9fe128bcdd693f
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-09-15 20:51:36 -0400

    first commiy

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void){
+
+    // variable declarations
+    int choice;
+    FILE *fptr;
+
+    //student record structure
+
+    // display main menu
+    printf("\t\tMAIN MENU\n"
+    "1. Create the binary file\n"
+    "2. Add a record to the file\n"
+    "3. Display the contents of the file\n"
+    "4. Seek a specific record\n"
+    "5. Update contents of a record\n"
+    "6. Delete a record for a specific name\n"
+    "7. Exit\n"
+    "\t\tPlease enter your choice...");
+
+    // get & store user's choice
+    scanf("%d", &choice);
+
+    
+    
+}
